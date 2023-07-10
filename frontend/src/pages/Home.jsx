39f1f1a178e70fbfde20c2cefb83ed94a01d975e
Destiny <dhowell831@gmail.com>
2023-07-09 21:24:47 -0400

    editing front end, having issues running npm run kickstart

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,8 @@
+import { Form, FormGroup, Input, Label, Button} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function HomePage() {
   return <>
-    <h1>PHOTOPHILE</h1>
+    <h1>ALBUMS</h1>
   </>;
 }
